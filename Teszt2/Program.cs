6dfbc987db791394bcbf7438a9e30dddda517494
Attirobert <karobatti@gmail.com>
2023-09-08 14:25:05 +0200

    Add project files.

---
+++ Teszt2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teszt 2 program");
+            Console.ReadKey();
+        }
+    }
+}
